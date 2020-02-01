a0fd0580301e1add4c057946b84098357768abd6
James Ravenscroft <web@jravenscroft.co.uk>
2020-02-01 17:29:50 +1300

    Docker

--- install.sh
+++ install.sh
@@ -9,10 +9,14 @@ brew cask install hyper
 brew cask install visual-studio-code
 brew cask install google-chrome
 brew cask install firefox
+brew cask install docker
 
 curl -o- https://raw.githubusercontent.com/jrwebdev/dev-environment/master/.gitconfig > ~/.gitconfig
 curl -o- https://raw.githubusercontent.com/jrwebdev/dev-environment/master/.zshrc > ~/.zshrc
 curl -o- https://raw.githubusercontent.com/jrwebdev/dev-environment/master/.hyper.js > ~/.hyper.js
+
+mkdir -p ~/.zsh/completion
+curl -L https://raw.githubusercontent.com/docker/compose/1.25.3/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
 
 npm install --global pure-prompt
 
